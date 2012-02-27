cf7c13a8827a493b54c8798af0175c9aa39d317f
agees <agees.agl@gmail.com>
2012-02-27 20:50:59 +0900

    modified:   app/controllers/hellotube_controller.rb

--- app/controllers/hellotube_controller.rb
+++ app/controllers/hellotube_controller.rb
@@ -1,9 +1,8 @@
-# encoding: utf-8
+# -*- encoding: utf-8 -*-
 require 'youtube_it'
 class HellotubeController < ApplicationController
   def index
     youtube = YouTubeIt::Client.new(:dev_key => "AI39si4YZPgfRKaeoyimigmQUH8Te9NB7nspTT-l97TKORLbg2x1cr2twO5kJKt4vQuWw29uI7wiyzbL-a-uxEdnoWLiZ6-Rqg")
     @videos = youtube.videos_by(:most_viewed).videos
-    @someJapanese = "あああ"
   end
 end
