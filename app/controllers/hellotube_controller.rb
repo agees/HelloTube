dde69f673fa4d60148b7b82ded7c0952fd46d550
agees <agees.agl@gmail.com>
2012-03-19 20:25:04 +0900

    modified:   app/controllers/hellotube_controller.rb 	modified:   app/views/hellotube/index.html.erb 	modified:   app/views/layouts/application.html.erb 	modified:   config/routes.rb 	app/assets/javascripts/videolists.js.coffee 	app/assets/stylesheets/scaffolds.css.scss 	app/assets/stylesheets/videolists.css.scss 	app/controllers/videolists_controller.rb 	app/helpers/videolists_helper.rb 	app/models/videolist.rb 	app/views/ 	db/migrate/ 	db/schema.rb 	test/fixtures/videolists.yml 	test/functional/videolists_controller_test.rb 	test/unit/helpers/videolists_helper_test.rb 	test/unit/videolist_test.rb

--- app/controllers/hellotube_controller.rb
+++ app/controllers/hellotube_controller.rb
@@ -4,6 +4,13 @@ require 'youtube_it'
 class HellotubeController < ApplicationController
   def index
     youtube = YouTubeIt::Client.new(:dev_key => "AI39si4YZPgfRKaeoyimigmQUH8Te9NB7nspTT-l97TKORLbg2x1cr2twO5kJKt4vQuWw29uI7wiyzbL-a-uxEdnoWLiZ6-Rqg")
-    @videos = youtube.videos_by(:most_viewed).videos
+    allvideos = Videolist.all
+    @videos = []
+    for video in allvideos
+      @videos << youtube.video_by(video.url)
+    end
+    @videourl = @videos[0].embed_url
+    @videotitle = allvideos[0].title
+    @videodesc = allvideos[0].description
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 HelloTube::Application.routes.draw do
+  resources :videolists
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
